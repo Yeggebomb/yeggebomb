297b2dcc967f30e3e38dbeb8455e38e40a0b2fbb
Jonathan Stanton <jonathan@jastanton.com>
2014-11-20 11:52:35 -0800

    no moar log lines

--- src/frontend/javascript/game.js
+++ src/frontend/javascript/game.js
@@ -52,9 +52,7 @@ game.Main = function() {
   /** @private {number} */
   this.globalTick_ = 0;
 
-  console.log('attach');
   this.attach();
-  console.log('init start');
   this.init();
   this.switchGameStateTo(game.Main.State.PENDING);
 };
@@ -128,7 +126,6 @@ game.Main.prototype.init = function() {
  * Initializes values to start game.
  */
 game.Main.prototype.startGame = function() {
-  console.log('start game');
   this.switchGameStateTo(game.Main.State.RECORDING);
   this.physicsLoop();
   this.renderLoop();
@@ -188,26 +185,18 @@ game.Main.prototype.switchGameStateTo = function(nextGameState) {
  */
 game.Main.prototype.physicsLoop = function() {
   var currTime = +new Date() / 1000;
-  //console.log('currtime in secs: ' + currTime);
   if (!this.tmpGameTime_) this.tmpGameTime_ = +new Date() / 1000;
   if (!this.lastTimeRan_) this.lastTimeRan_ = +new Date() / 1000;
   if (!this.physicsRemainderTime_) this.physicsRemainderTime_ = 0;
 
-  //console.log('currtime - time: ' + (currTime - this.lastTimeRan_));
   var dt = (currTime - this.lastTimeRan_) + this.physicsRemainderTime_;
-
-  //console.log('dt: ' + dt);
-
   var dtstep = 1 / game.Main.FPS;
   var steps = Math.floor(dt / dtstep);
+
   this.physicsRemainderTime_ = dt - dtstep * steps;
-
-  //console.log('num steps: ' + steps);
-
   this.camera_.update();
   // Update loop
   for (var step = 0; step < steps; step++) {
-    //console.log('performing step: ' + step);
     this.gameStateAdvancer(this.globalTick_);
 
     game.core.Entity.forEach(function(entity) {
@@ -281,7 +270,7 @@ game.Main.prototype.stateChangeToRecording = function() {
 
       var endPosition = entity.endPosition;
       if (endPosition) {
-        console.log('error of this much',
+        console.log('error of this much:',
             entity.getPosition().distanceTo(endPosition));
       }
       entity.ignoreKeys(false);
@@ -343,7 +332,6 @@ game.Main.prototype.stateChangeToPlayback = function() {
  * Login
  */
 game.Main.prototype.loginCallback = function() {
-  console.log('login callback');
   this.firebase_.authWithOAuthPopup('google', function(error, authData) {
     if (error) {
       console.warn(error);
@@ -367,9 +355,7 @@ game.Main.prototype.loginCallback = function() {
  *
  * @param {Object} snapshot
  */
-game.Main.prototype.onRetrieveEvents = function(snapshot) {
-  // console.log(snapshot);
-};
+game.Main.prototype.onRetrieveEvents = function(snapshot) {};
 
 
 /**
